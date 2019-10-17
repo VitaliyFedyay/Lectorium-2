d273ce6acf1741ddb4aa48c728158eaeae2e80f9
Федяй Виталий <vfedyay@hold.bayadera.ua>
2019-10-17 17:32:01 +0300

    task 2 completed

--- task2/main.js
+++ task2/main.js
@@ -1,33 +1,40 @@
-var nominal = [1, 2, 5, 10, 20, 50]
+const coins = [
+  { name: '100 UAH', value: 100 },
+  { name: '50 UAH', value: 50 },
+  { name: '20 UAH', value: 20 },
+  { name: '10 UAH', value: 10 },
+  { name: '5 UAH', value: 5 },
+  { name: '2 UAH', value: 2 },
+  { name: '1 UAH', value: 1 },
+]
+const coinCount = {}
 
-/* Использовал известный с 14 века нерекурсивный алгоритм индуса Нарайана Пандита.
- Из любого порядка в массиве генерится следующая итерация,
-  учитывая лексикографический (по алфавиту) порядок сортировки.*/
-  
-function nextLexInPlace(arr) {
-  var i, a = -1, b = -1;
-  for (i = 0; i < arr.length - 1; i++) if (arr[i] < arr[1 + i]) a = i;
-  if (!~a) return; // no more permutations
-  for (i = a + 1; i < arr.length; i++) if (arr[a] < arr[i]) b = i;
-  swap(arr, a, b);
-  a++;
-  b = arr.length - 1;
-  while (a < b) swap(arr, a++, b--);
-  return true;
+submit.onclick = function () {
+  let money = document.getElementById('money').value
+  let price = document.getElementById('price').value
+  let diff = money - price
+  document.getElementById('diff').innerHTML = diff
+  generateCoinChange(diff)
 }
 
-function swap(arr, a, b) {
-  var xx = arr[a];
-  arr[a] = arr[b];
-  arr[b] = xx;
+function generateCoinChange(diff) {
+  let indexOfCoin = 0
+  while (diff > 0) {
+    const coin = coins[indexOfCoin]
+    if (diff >= coin.value) {
+      diff -= coin.value
+      coinCount[coin.name] = coinCount[coin.name] == null ? 1 : coinCount[coin.name] + 1
+    } else {
+      indexOfCoin++
+    }
+  }
+
+  const text = Object.getOwnPropertyNames(coinCount)
+    .map(coinName => coinName + ": " + coinCount[coinName] + ' amount')
+    .join(`, `)
+  document.getElementById('optimal').innerHTML = text
+
 }
 
-var combi = function allMutations(source) {
-  var result = [], arr = source.slice();
-  result.push(arr.sort().slice());
-  while (nextLexInPlace(arr)) result.push(arr.slice());
-  return result;
-}
 
-console.dir(combi(nominal))
-console.log(combi[500])
+
